1874026b30a0bf3c3543be782f93713d1d9d171e
FAYEMI BOLUWATIFE <67077455+BOVAGE@users.noreply.github.com>
2021-01-03 11:15:51 -1200

    gotoTop arrow working

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
 let sidebar = document.querySelector(".nav-items");
+let gotoTop = document.querySelector(".fa-arrow-up");
+
+/* When the user scrolls down 100px from the 
+top of the document, show the button */
+window.onscroll = () => scrollFunction();
+
+function scrollFunction () {
+	if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+		gotoTop.style.display = "block";
+		console.log("show");
+	} else {
+		gotoTop.style.display = "none";
+		console.log("hide");
+	}
+}
+
+/* when the user clicks the arrow up, 
+scroll to the top of the document */
+gotoTop.addEventListener("click", () => {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+})
 
 document.querySelector(".menu").addEventListener("click", () => {
 	sidebar.classList.toggle("show");
